62d48fc941d10460c966bd5722a25b45bd15788f
Erickkirui <40591965+Erickkirui@users.noreply.github.com>
2023-05-03 17:24:27 +0300

    update

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GameList from './GameList';
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Basketball Schedule</h1>
+      <GameList  />
+    </div>
+  );
+}
+
+export default App;
